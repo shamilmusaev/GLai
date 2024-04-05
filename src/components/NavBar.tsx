848414b74754b03ca6b8541af5b0717a53a476a2
Shamil Musaev <dwhitewolf@Shamils-MacBook-Pro.local>
2024-04-05 21:30:03 +0200

    created the project, created the navbar and hero section, connected to prismic

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Content } from "@prismicio/client";
+import WordMark from "./WordMark";
+import Link from "next/link";
+import { PrismicNextLink } from "@prismicio/next";
+import ButtonLink from "@/components/ButtonLink";
+
+type NavBarProps = {
+  settings: Content.SettingsDocument;
+};
+
+export default function NavBar({ settings }: NavBarProps) {
+  return (
+    <nav className="md-:py6 px-4 py-4 md:px-6" aria-label="Main">
+      <div className="  mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
+        <Link href="/">
+          <WordMark />
+          <span className="sr-only">Glisten.ai Home Page</span>
+        </Link>
+
+        <ul className="flex gap-6">
+          {settings.data.navigation.map((item) => {
+
+            if( item.cta_button){
+                return (
+                    <li key={item.label}>
+                    <ButtonLink key={item.label} field={item.link}> 
+                        {item.label}
+                    </ButtonLink>
+                    </li>
+                )
+            }
+            return (
+            <li key={item.label}>
+              <PrismicNextLink
+                className="inline-flex min-h-11 items-center"
+                field={item.link}
+              >
+                {item.label}
+              </PrismicNextLink>
+            </li>
+          )})}
+        </ul>
+      </div>
+    </nav>
+  );
+}
