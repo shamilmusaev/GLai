a0d28b719b7db90c3f2477caf36053833e2b7995
Shamil Musaev <dwhitewolf@Shamils-MacBook-Pro.local>
2024-04-16 13:21:36 +0200

    added mobile hamburger menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,48 +1,125 @@
 "use client";
 
-import { Content } from "@prismicio/client";
+import { useState } from "react";
+import { Content, asLink } from "@prismicio/client";
 import WordMark from "./WordMark";
 import Link from "next/link";
 import { PrismicNextLink } from "@prismicio/next";
 import ButtonLink from "@/components/ButtonLink";
+import { MdClose, MdMenu } from "react-icons/md";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 type NavBarProps = {
   settings: Content.SettingsDocument;
 };
 
 export default function NavBar({ settings }: NavBarProps) {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
   return (
     <nav className="md-:py6 px-4 py-4 md:px-6" aria-label="Main">
       <div className="  mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
-        <Link href="/">
-          <WordMark />
-          <span className="sr-only">Glisten.ai Home Page</span>
-        </Link>
+        <div className="flex items-center justify-between">
+          <Link href="/" className="z-50" onClick={() => setOpen(false)}>
+            <WordMark />
+            <span className="sr-only">Glisten.ai Home Page</span>
+          </Link>
+          <button
+            type="button"
+            className="block p-2 text-3xl text-white md:hidden"
+            aria-expanded={open}
+            onClick={() => setOpen(true)}
+          >
+            <MdMenu />
+            <span className="sr-only">Open Menu</span>
+          </button>
+        </div>
 
-        <ul className="flex gap-6">
-          {settings.data.navigation.map((item) => {
+        {/* Mobile Nav */}
 
-            if( item.cta_button){
+        <div
+          className={clsx(
+            "item-end fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col gap-4 bg-[#070815] pr-4 pt-14 transition-transform duration-300 ease-in-out motion-reduce:transition-none md:hidden",
+            open ? "translate-x-0" : "translate-x-[100%]",
+          )}
+        >
+          <button
+            type="button"
+            className="fixed right-4 top-4 mb-4 block p-2 text-3xl text-white md:hidden"
+            aria-expanded={open}
+            onClick={() => setOpen(false)}
+          >
+            <MdClose />
+            <span className="sr-only">Close Menu</span>
+          </button>
+
+          <div className="grid justify-items-end gap-8">
+            {settings.data.navigation.map((item) => {
+              if (item.cta_button) {
                 return (
-                    <li key={item.label}>
-                    <ButtonLink key={item.label} field={item.link}> 
-                        {item.label}
-                        
-                    </ButtonLink>
-                    
-                    </li>
-                )
+                  <ButtonLink
+                    key={item.label}
+                    field={item.link}
+                    onClick={() => setOpen(false)}
+                    aria-current={
+                      pathname.includes(asLink(item.link) as string)
+                        ? "page"
+                        : undefined
+                    }
+                  >
+                    {item.label}
+                  </ButtonLink>
+                );
+              }
+              return (
+                <PrismicNextLink
+                  key={item.label}
+                  className="block px-3 text-3xl first:mt-8"
+                  field={item.link}
+                  onClick={() => setOpen(false)}
+                  aria-current={
+                    pathname.includes(asLink(item.link) as string)
+                      ? "page"
+                      : undefined
+                  }
+                >
+                  {item.label}
+                </PrismicNextLink>
+              );
+            })}
+          </div>
+        </div>
+
+        {/* Desktop Nav */}
+
+        <ul className="flex hidden gap-6 md:flex">
+          {settings.data.navigation.map((item) => {
+            if (item.cta_button) {
+              return (
+                <li key={item.label}>
+                  <ButtonLink key={item.label} field={item.link}>
+                    {item.label}
+                  </ButtonLink>
+                </li>
+              );
             }
             return (
-            <li key={item.label}>
-              <PrismicNextLink
-                className="inline-flex min-h-11 items-center"
-                field={item.link}
-              >
-                {item.label}
-              </PrismicNextLink>
-            </li>
-          )})}
+              <li key={item.label}>
+                <PrismicNextLink
+                  className="inline-flex min-h-11 items-center"
+                  aria-current={
+                    pathname.includes(asLink(item.link) as string)
+                      ? "page"
+                      : undefined
+                  }
+                  field={item.link}
+                >
+                  {item.label}
+                </PrismicNextLink>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
